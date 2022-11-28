16cfb8a60d85be0e31d1e2ffd144a96533231c49
DeepankarKarn <119203668+DeepankarKarn@users.noreply.github.com>
2022-11-28 07:46:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ http.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 })
 
-app.use(express.static(__dirname + '/public'))
+//app.use(express.static(__dirname + '/public'))
 
 app.get('/',(req,res) =>{
     res.sendFile(__dirname + '/index.html')
